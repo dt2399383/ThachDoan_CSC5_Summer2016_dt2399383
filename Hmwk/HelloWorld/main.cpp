9c37b88ad5d0a5e234c9198f97fcedb0e42a5402
unknown <dthach2@student.rcc.edu>
2016-06-24 02:50:47 -0700

    Signed-off-by: unknown <dthach2@student.rcc.edu>

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Doan Thach 
+ * Created on June 23, 2016, 2:45am
+ * Purpose: HelloWorld for Github
+ */
+
+//System Libraries
+#include <iostream> //Input/Output
+using namespace std;//Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    cout<<"Hello World!"<<endl;
+    //Output the processed data
+    
+    //Exit Stage Right!
+    return 0;
+}
+
